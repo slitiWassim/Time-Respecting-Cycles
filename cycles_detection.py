a304fb08abbd279b446624e6824609041171c4d3
Wassim <wassimsliti1998@gmail.com>
2025-10-10 15:34:44 +0200

    Time Respecting Cycles

---
+++ cycles_detection.py
@@ -0,0 +1,170 @@
+import os 
+import sys
+import time
+
+import pandas as pd
+import numpy as np 
+
+from raphtory import algorithms as rp
+from raphtory import Graph
+
+
+from collections import defaultdict
+from bisect import bisect_right
+
+
+
+
+def time_respecting_cycles(rolling_g, max_length=None, max_cycles=None, max_duration=None):
+    """
+
+    Enumerate all time-respecting simple cycles in a temporal graph with duration less 'max_duration'.
+
+    Parameters
+    ----------
+    rolling_g : Raphtory Graph 
+        Raphtory Temporal Knowledge Graph
+    max_length : int, optional
+        Maximum number of nodes in a cycle (to prune long cycles).
+    max_cycles : int, optional
+        Stop after this many cycles have been found (Prevent RAM overload).
+    max_duration in (ms) : int , optional
+        Only return cycles with duration less than max_duration
+            
+        
+    Yields
+    ------
+    (cycle_nodes, cycle_timestamps)
+        Lists of node names and edges timestamps in temporal chronological order.( Time Respecting Cycles)
+    """
+
+    cycle_count = [0]
+    adjacency = defaultdict(dict)
+
+    # ---- Build adjacency with sorted edge timestamps ----
+    for node in rolling_g.nodes:
+        for edge in node.out_edges:
+            times = sorted(edge.history().tolist())
+            if times:
+                adjacency[node.name][edge.dst.name] = times
+
+    # ---- Temporal neighbor selection ----
+    def _out_neighbors(node_id, prev_time):
+        neighbors = []
+        for nbr, times in adjacency[node_id].items():
+            if prev_time is None:
+                for t in times:
+                    neighbors.append((nbr, t))
+            else:
+                idx = bisect_right(times, prev_time)
+                for t in times[idx:]:
+                    neighbors.append((nbr, t))
+        return neighbors
+
+    # ---- Strongly Connected Components ----
+    def strongly_connected_components():
+        index, lowlink = {}, {}
+        stack, on_stack = [], set()
+        sccs, idx = [], 0
+
+        def dfs(v):
+            nonlocal idx
+            index[v] = lowlink[v] = idx
+            idx += 1
+            stack.append(v)
+            on_stack.add(v)
+
+            for w in adjacency.get(v, {}).keys():
+                if w not in index:
+                    dfs(w)
+                    lowlink[v] = min(lowlink[v], lowlink[w])
+                elif w in on_stack:
+                    lowlink[v] = min(lowlink[v], index[w])
+
+            if lowlink[v] == index[v]:
+                comp = set()
+                while True:
+                    w = stack.pop()
+                    on_stack.remove(w)
+                    comp.add(w)
+                    if w == v:
+                        break
+                sccs.append(comp)
+
+        for v in list(adjacency.keys()):
+            if v not in index:
+                dfs(v)
+
+        return sccs
+
+    # ---- Johnson-like temporal cycle search with early duration pruning ----
+    def _johnson_cycle_search(start):
+        path, times_path = [start], []
+        blocked = set()
+        B = defaultdict(set)
+
+        def unblock(u):
+            for v in list(B[u]):
+                if v in blocked:
+                    blocked.remove(v)
+                    unblock(v)
+            B[u].clear()
+
+        def backtrack(v, last_time):
+            blocked.add((v, last_time))
+            closed = False
+
+            for w, t_next in _out_neighbors(v, last_time):
+                # ---- Early duration pruning ----
+                if times_path and max_duration is not None:
+                    if t_next - times_path[0] > max_duration:
+                        continue  # skip: would exceed allowed duration
+
+                if w == start:
+                    if times_path:
+                        first_t = times_path[0]
+                        duration = t_next - first_t
+                    else:
+                        duration = 0
+
+                    if (
+                        (max_length is None or len(path) <= max_length)
+                        and (max_duration is None or duration <= max_duration)
+                    ):
+                        cycle_count[0] += 1
+                        yield (path[:] + [start], times_path[:] + [t_next])
+                        closed = True
+                        if max_cycles and cycle_count[0] >= max_cycles:
+                            return
+                elif (w, t_next) not in blocked and (max_length is None or len(path) < max_length):
+                    path.append(w)
+                    times_path.append(t_next)
+                    for result in backtrack(w, t_next):
+                        yield result
+                        closed = True
+                        if max_cycles and cycle_count[0] >= max_cycles:
+                            return
+                    path.pop()
+                    times_path.pop()
+
+            if closed:
+                unblock((v, last_time))
+            else:
+                for w, _ in _out_neighbors(v, None):
+                    B[(w, None)].add((v, last_time))
+
+            if (v, last_time) in blocked:
+                blocked.remove((v, last_time))
+
+        yield from backtrack(start, None)
+
+    # ---- SCC-based search ----
+    sccs = strongly_connected_components()
+    components = [c for c in sccs if len(c) > 1]
+
+    for comp in components:
+        for start in list(comp):
+            for cycle in _johnson_cycle_search(start):
+                yield cycle
+                if max_cycles and cycle_count[0] >= max_cycles:
+                    return
